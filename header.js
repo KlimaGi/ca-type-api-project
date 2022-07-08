34a492dd4bbe9baeaf05551dd6f15dbbae241c3a
Giedre <giedre.klima@gmail.com>
2022-07-08 11:20:24 +0300

    header, posts

---
+++ header.js
@@ -0,0 +1,22 @@
+let bodyEl = document.querySelector("body");
+let headerEl = document.createElement("div");
+headerEl.classList.add("header");
+
+let HomeEl = document.createElement("a");
+HomeEl.textContent = "Home";
+HomeEl.href = "./index.html";
+
+let UsersEl = document.createElement("a");
+UsersEl.textContent = "Users";
+UsersEl.href = "./users.html";
+
+let AlbumsEl = document.createElement("a");
+AlbumsEl.textContent = "Albums";
+AlbumsEl.href = "./albums.html";
+
+let PostsEl = document.createElement("a");
+PostsEl.textContent = "Posts";
+PostsEl.href = "./posts.html";
+
+headerEl.append(HomeEl, UsersEl, AlbumsEl, PostsEl);
+bodyEl.prepend(headerEl);
---
+++ posts.js
@@ -0,0 +1,46 @@
+let queryParams = document.location.search;
+let urlParams = new URLSearchParams(queryParams);
+let userId = urlParams.get("user_id");
+
+let postsWrapperEl = document.querySelector("#posts-wrapper");
+let postsListTitle = document.createElement("h2");
+let postsList = document.createElement("ul");
+
+postsWrapperEl.append(postsListTitle, postsList);
+
+if (userId) {
+  fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
+    .then((res) => res.json())
+    .then((posts) => {
+      fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        .then((res) => res.json())
+        .then((user) => {
+          console.log(user);
+          postsListTitle.textContent = `Posts of ${user.name}:`;
+
+          posts.map((post) => {
+            let postItem = document.createElement("li");
+            postItem.innerHTML = `<a href="./post.html?post_id=${post.id}">${post.title}</a>`;
+
+            postsList.prepend(postItem);
+          });
+        });
+    });
+} else {
+  fetch(`https://jsonplaceholder.typicode.com/posts`)
+    .then((res) => res.json())
+    .then((posts) => {
+      postsListTitle.textContent = "All Posts: ";
+
+      posts.map((post) => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+          .then((res) => res.json())
+          .then((user) => {
+            let postItem = document.createElement("li");
+            postItem.innerHTML = `<a href="./post.html?post_id=${post.id}">${post.title} (${user.name})</a>`;
+
+            postsList.prepend(postItem);
+          });
+      });
+    });
+}
