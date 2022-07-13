c29497a7c7eb21d38aebf25373a6d7639547747a
Giedre <giedre.klima@gmail.com>
2022-07-13 16:23:30 +0300

    use embed in users.js, refactor

--- users.js
+++ users.js
@@ -1,24 +1,24 @@
-fetch(`https://jsonplaceholder.typicode.com/users`)
-  .then((res) => res.json())
-  .then((users) => {
-    let usersWrapper = document.querySelector("#users-wrapper");
-    let usersList = document.createElement("ul");
-    usersList.classList.add("ul-el");
-    usersWrapper.append(usersList);
+function usersPageInit() {
+  fetch(`https://jsonplaceholder.typicode.com/users?_embed=posts`)
+    .then((res) => res.json())
+    .then((users) => {
+      let usersWrapper = document.querySelector("#users-wrapper");
+      let usersList = document.createElement("ul");
+      usersList.classList.add("ul-el");
 
-    users.map((user) => {
-      fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
-        .then((res) => res.json())
-        .then((posts) => {
-          let postCount = posts.length;
+      usersWrapper.append(usersList);
 
-          let pluralS = postCount > 1 ? "s" : "";
+      users.map((user) => {
+        let postCount = user.posts.length;
+        let pluralS = postCount > 1 ? "s" : "";
 
-          let userEl = document.createElement("li");
-          userEl.classList.add("li-el");
-          userEl.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name} (${postCount} post${pluralS})</a>`;
+        let userEl = document.createElement("li");
+        userEl.classList.add("li-el");
 
-          usersList.prepend(userEl);
-        });
+        userEl.innerHTML = `<a href="./user.html?user_id=${user.id}">${user.name} (${postCount} post${pluralS})</a>`;
+
+        usersList.prepend(userEl);
+      });
     });
-  });
+}
+usersPageInit();
