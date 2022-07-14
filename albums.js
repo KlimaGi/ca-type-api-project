5aa01353129426ed206f7e8e2a16efab12569fa5
Giedre <giedre.klima@gmail.com>
2022-07-14 18:33:52 +0300

    abums refactor, corrections

--- albums.js
+++ albums.js
@@ -1,9 +1,11 @@
 let albumsWrapperEl = document.querySelector("#albums-wrapper");
+let albumsWrapperTitle = document.querySelector("#albums-wrapper-title");
 
 function albumsInit() {
   let queryParams = document.location.search;
   let urlParams = new URLSearchParams(queryParams);
   let userId = urlParams.get("user_id");
+
   if (userId) {
     renderAlbumsByUserId(userId);
   } else {
@@ -18,6 +20,22 @@ function renderAlbumsByUserId(id) {
     .then((res) => res.json())
     .then((singleAlbum) => {
       console.log("singleAlbum", singleAlbum);
+
+      let albumItem = document.createElement("div");
+      albumItem.classList.add("album-item");
+
+      albumsWrapperTitle.textContent = `Albums of ${singleAlbum.user.name}`;
+
+      let albumTitle = document.createElement("h3");
+      let capitalizeTitle = capitalize(album.title);
+
+      albumTitle.innerHTML = `<a class="link" href="./album.html?album_id=${album.id}&album_title=${album.title}&user_id=${album.userId}&user_name=${album.user.name}">${capitalizeTitle} (${album.photos.length})</a>`;
+
+      let imgEl = document.createElement("img");
+      imgEl.src = `${album.photos[0].thumbnailUrl}`;
+
+      albumItem.append(imgEl, albumTitle);
+      albumsWrapperEl.prepend(albumItem);
     });
 }
 
@@ -28,14 +46,27 @@ function renderAllAlbums() {
     .then((res) => res.json())
     .then((albums) => {
       albums.map((singleAlbum) => {
-        console.log(singleAlbum);
+        let albumData = {
+          album: singleAlbum,
+          title: "All albums:",
+          userHref: `./user.html?user_id=${singleAlbum.user.id}`,
+          userFullName: singleAlbum.user.name,
+        };
+        renderSingleAlbum(albumData);
 
-        renderSingleAlbum(singleAlbum);
+        // renderSingleAlbum({
+        //   album: singleAlbum,
+        //   title: "All albums:",
+        //   userHref: `./user.html?user_id=${singleAlbum.user.id}`,
+        //   userFullName: singleAlbum.user.name,
+        // });
       });
     });
 }
 
-function renderSingleAlbum(album) {
+function renderSingleAlbum(data) {
+  let { album, title, userHref, userFullName } = data;
+
   let albumItem = document.createElement("div");
   albumItem.classList.add("album-item");
 
@@ -45,12 +76,13 @@ function renderSingleAlbum(album) {
   albumTitle.innerHTML = `<a class="link" href="./album.html?album_id=${album.id}&album_title=${album.title}&user_id=${album.userId}&user_name=${album.user.name}">${capitalizeTitle} (${album.photos.length})</a>`;
 
   let albumAuthor = document.createElement("a");
-  albumAuthor.textContent = `Album created by: ${album.user.name}`;
-  albumAuthor.href = `./user.html?user_id=${album.user.id}`;
+  albumAuthor.textContent = `Album created by: ${userFullName}`;
+  albumAuthor.href = userHref;
 
   let imgEl = document.createElement("img");
   imgEl.src = `${album.photos[0].thumbnailUrl}`;
 
+  albumsWrapperTitle.textContent = title;
   albumItem.append(imgEl, albumTitle, albumAuthor);
   albumsWrapperEl.prepend(albumItem);
 }
