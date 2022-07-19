00463a712dc8d3f45fd657a0d719db34d5499907
Giedre <giedre.klima@gmail.com>
2022-07-19 20:13:44 +0300

    create post data render to the screen

--- createPost.js
+++ createPost.js
@@ -1,4 +1,5 @@
 import { headerView } from "./headerView.js";
+import { capitalize } from "./functions.js";
 
 headerView();
 
@@ -15,8 +16,10 @@ let usersData = users.map((user) => {
 console.log("usersData", usersData);
 
 let userIdByName = (name) => usersData.find((item) => item.userName === name);
+let userNameById = (id) => usersData.find((item) => item.userId === Number(id));
 
 console.log("userIdByName", userIdByName("Patricia Lebsack"));
+console.log("userNameById", userNameById(3));
 
 let selectOptions = document.querySelector("#create-post-user");
 usersData.map((user) => {
@@ -31,12 +34,55 @@ let createPostForm = document.querySelector("#create-post-form");
 
 createPostForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  let title = event.target.elements["post-title"].value;
+  let titleInput = event.target.elements["post-title"].value;
 
-  let body = event.target.elements["post-body"].value;
+  let bodyInput = event.target.elements["post-body"].value;
 
   let userNameValue = event.target.elements["create-post-user"].value;
   console.log("userName", userNameValue);
+  // ---------------------------
   let userIdNumber = userIdByName(userNameValue);
   console.log("userIdNumber", userIdNumber);
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: titleInput,
+      body: bodyInput,
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log("json", json);
+      renderCreatedPost(json);
+    });
 });
+
+function renderCreatedPost(data) {
+  let { title, body, userId } = data;
+
+  let author = userNameById(userId);
+  console.log("author", author);
+
+  let newPostEl = document.querySelector("#new-post");
+  let postTitleEl = document.createElement("h3");
+  postTitleEl.classList.add("page-title");
+
+  postTitleEl.textContent = capitalize(title);
+
+  let postBodyEl = document.createElement("p");
+
+  postBodyEl.textContent = capitalize(body);
+
+  let postAuthorEl = document.createElement("p");
+
+  postAuthorEl.textContent = "Author is: " + author.userName;
+
+  newPostEl.append(postTitleEl, postBodyEl, postAuthorEl);
+}
+
+renderCreatedPost();
