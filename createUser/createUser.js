db8e76a9c4163d310331cd0556b49fc280fb4090
Giedre <giedre.klima@gmail.com>
2022-07-20 22:34:38 +0300

    create user get result from api

--- createUser/createUser.js
+++ createUser/createUser.js
@@ -1,6 +1,68 @@
 import { headerView } from "../headerView.js";
 
-headerView();
+function createUserInit() {
+  headerView();
+
+  let createUserForm = document.querySelector("#create-user-form");
+  createUserForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    let name = event.target.elements["full-name"].value;
+    let username = event.target.elements.username.value;
+    let email = event.target.elements.email.value;
+
+    let street = event.target.elements.street.value;
+    let suite = event.target.elements.suite.value;
+    let city = event.target.elements.city.value;
+
+    let zipcode = event.target.elements.zipcode.value;
+    let lat = event.target.elements.lat.value;
+    let lng = event.target.elements.lng.value;
+
+    let phone = event.target.elements.phone.value;
+    let website = event.target.elements.website.value;
+    let companyName = event.target.elements.company.value;
+
+    let catchPhrase = event.target.elements["catch-phrase"].value;
+    let bs = event.target.elements.bs.value;
+
+    let newUser = {
+      id: 11,
+      name,
+      username,
+      email,
+      address: {
+        street,
+        suite,
+        city,
+        zipcode,
+        geo: {
+          lat,
+          lng,
+        },
+      },
+      phone,
+      website,
+      company: {
+        name: companyName,
+        catchPhrase,
+        bs,
+      },
+    };
+
+    let res = await fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "POST",
+      body: JSON.stringify(newUser),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    let createUser = await res.json();
+    console.log("createUser", createUser);
+  });
+}
+
+createUserInit();
 
 let user = {
   id: 1,
