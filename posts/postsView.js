276b34efd1569771b4e2ac84e8be9bb60bd2f682
Giedre <giedre.klima@gmail.com>
2022-07-21 23:03:07 +0300

    refactor posts --> mvc

--- posts/postsView.js
+++ posts/postsView.js
@@ -1,3 +1,66 @@
-function renderPostsView() {}
+import { capitalize } from "../functions.js";
 
-export { renderPostsView };
+function renderPostsByUserId(user) {
+  let { posts, name } = user;
+
+  let postsWrapperEl = document.querySelector("#posts-wrapper");
+  let titleAndBtnWrapperEl = document.createElement("div");
+  titleAndBtnWrapperEl.classList.add("title-btn-wrapper");
+
+  let postsListTitle = document.createElement("h2");
+  postsListTitle.classList.add("page-title");
+  postsListTitle.textContent = `Posts of ${name}:`;
+
+  let createPostBtnEl = document.createElement("a");
+  createPostBtnEl.textContent = "Create Post";
+  createPostBtnEl.classList.add("link-btn");
+  createPostBtnEl.setAttribute("href", "./create-post.html");
+
+  let postsList = document.createElement("ul");
+  postsList.classList.add("ul-el");
+
+  titleAndBtnWrapperEl.append(postsListTitle, createPostBtnEl);
+  postsWrapperEl.append(titleAndBtnWrapperEl, postsList);
+
+  posts.map((post) => {
+    let postItem = document.createElement("li");
+    postItem.classList.add("li-el");
+    let titleCapitalize = capitalize(post.title);
+    postItem.innerHTML = `<a href="./post.html?post_id=${post.id}">${titleCapitalize}</a>`;
+
+    postsList.prepend(postItem);
+  });
+}
+
+function renderAllPostsList(posts) {
+  let postsWrapperEl = document.querySelector("#posts-wrapper");
+  let titleAndBtnWrapperEl = document.createElement("div");
+  titleAndBtnWrapperEl.classList.add("title-btn-wrapper");
+
+  let postsListTitle = document.createElement("h2");
+  postsListTitle.classList.add("page-title");
+  postsListTitle.textContent = "All Posts: ";
+
+  let createPostBtnEl = document.createElement("a");
+  createPostBtnEl.textContent = "Create Post";
+  createPostBtnEl.classList.add("link-btn");
+  createPostBtnEl.setAttribute("href", "./create-post.html");
+
+  let postsList = document.createElement("ul");
+  postsList.classList.add("ul-el");
+
+  titleAndBtnWrapperEl.append(postsListTitle, createPostBtnEl);
+  postsWrapperEl.append(titleAndBtnWrapperEl, postsList);
+
+  posts.map((post) => {
+    let { title, id, user } = post;
+    let postItem = document.createElement("li");
+    postItem.classList.add("li-el");
+    let titleCapitalize = capitalize(title);
+    postItem.innerHTML = `<a href="./post.html?post_id=${id}">${titleCapitalize} (${user.name})</a>`;
+
+    postsList.prepend(postItem);
+  });
+}
+
+export { renderPostsByUserId, renderAllPostsList };
